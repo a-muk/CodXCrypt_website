78902e0e96d9d9556529ea8001b29a3e02437bd2
a-muk <tintinm2017@gmail.com>
2021-10-01 21:45:48 +0530

    changed the go to top button to a circular button with an arrow in it

--- js/main.js
+++ js/main.js
@@ -251,9 +251,22 @@
 
 })(jQuery);
 
+var mybutton = document.getElementById("myBtn");
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
 	document.body.scrollTop = 0;
 	document.documentElement.scrollTop = 0;
   }
+
